970443b70556d8910f4009ce09ac8811e01a2ab2
Jonas <jns.lima@outlook.com>
2023-05-19 11:53:19 -0300

    variáveis

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+console.log("testando")
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = "Jonas";
+console.log(nomePessoa);
+
 var nome = "Jonas";
 console.log(typeof(nome));
 console.log(nome.toLocaleUpperCase());
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+    console.log(var1, var2, var3);
