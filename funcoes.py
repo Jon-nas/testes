31b8d4b9a28b7121d8f6844f6d7498dc941b662b
Jonas <jns.lima@outlook.com>
2023-05-24 19:57:56 -0300

    subpograma aninhado

--- funcoes.py
+++ funcoes.py
@@ -129,3 +129,23 @@ x = 0
 func1()
 func2()
 print(f'Programa principal - x = {x}')
+
+
+
+def taximetro(distancia):
+    def calculaMult():
+        if distancia < 5:
+            return 1.2
+        else:
+            return 1
+
+    multiplicador = calculaMult()
+    largada = 3
+    km_rodado = 2
+    valor = (largada + distancia * km_rodado) * multiplicador
+    return valor
+
+
+dist = eval(input("Entre com a distancia a ser percorrida em km: \n"))
+pagamento = taximetro(dist)
+print(f'O valor a pagar é R$ {pagamento}')
